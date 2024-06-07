ffc475bd7d9f2d671130a751d7535fce70a58a96
zabop <szabopal96@gmail.com>
2024-06-07 18:17:29 +0200

    build and push

---
+++ part3/ex3/builder.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o errexit   # abort on nonzero exitstatus
+set -o nounset   # abort on unbound variable
+set -o pipefail  # don't hide errors within pipes
+
+github_repo=$1
+dockerhub_repo=$2
+
+rm -rf dst
+mkdir dst
+
+echo "Specified GitHub repository: ${github_repo}"
+echo "Specified DockerHub repo:    ${dockerhub_repo}"
+
+git clone "git@github.com:${github_repo}.git" dst
+docker build --no-cache --tag "${dockerhub_repo}" dst/
+docker push "${dockerhub_repo}"
