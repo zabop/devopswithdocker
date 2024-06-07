3b7d3dcc74b977fc23196242efdaf962b80b169f
zabop <szabopal96@gmail.com>
2024-06-07 18:18:15 +0200

    less fluff

--- part3/ex3/builder.sh
+++ part3/ex3/builder.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-set -o errexit   # abort on nonzero exitstatus
-set -o nounset   # abort on unbound variable
-set -o pipefail  # don't hide errors within pipes
+set -o errexit
+set -o nounset
+set -o pipefail
 
 github_repo=$1
 dockerhub_repo=$2
